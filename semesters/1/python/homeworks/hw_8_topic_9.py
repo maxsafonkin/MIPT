52959e6e8e38dec3cf70b687e23890e4a3c2b42e
maxsafonkin <maxsaff0110@gmail.com>
2025-11-07 15:06:57 +0300

    Updated hw 8 for topic 9

--- semesters/1/python/homeworks/hw_8_topic_9.py
+++ semesters/1/python/homeworks/hw_8_topic_9.py
@@ -1,12 +1,44 @@
+import typing as t
 import re
 import uvicorn
 from datetime import date
 import uuid
-from pydantic import BaseModel, StrictStr, field_validator
+from pydantic import BaseModel, StrictStr, field_validator, ValidationInfo
 from fastapi import FastAPI, responses
 
 
 app = FastAPI()
+
+
+class Validator:
+    def __init__(self, pattern: str):
+        self._pattern: re.Pattern = re.compile(pattern)
+
+    def validate(self, value: t.Any) -> t.Any:
+        if not self._pattern.fullmatch(str(value)):
+            raise ValueError
+
+
+PATTERNS = {
+    "cyrillic_capitalized": r"[А-Я][а-я]+",
+    "phone": r"\+7[0-9]{10}",
+    "email": r"[\w\.]+@[\w]+\.[\w]+",
+}
+FIELD_TO_PATTERN_NAME = {
+    "name": "cyrillic_capitalized",
+    "last_name": "cyrillic_capitalized",
+    "phone": "phone",
+    "email": "email",
+}
+
+
+def create_validator(field_name: str) -> Validator:
+    return Validator(pattern=PATTERNS[FIELD_TO_PATTERN_NAME[field_name]])
+
+
+FIELD_VALIDATORS = {
+    field: create_validator(field) for field in ["name", "last_name", "phone", "email"]
+}
 
 
 class Appeal(BaseModel):
@@ -16,28 +48,10 @@ class Appeal(BaseModel):
     phone: StrictStr
     email: StrictStr
 
-    @field_validator("name", "last_name")
-    def validate_name(cls, value) -> str:
-        CYRILLIC_PATTERN = re.compile("[А-Я][а-я]+")
-        if not CYRILLIC_PATTERN.fullmatch(value):
-            raise ValueError
-
-        return value
-
-    @field_validator("phone")
-    def validate_phone(cls, value: str) -> str:
-        PHONE_PATTERN = re.compile(r"\+7[0-9]{10}")
-        if not PHONE_PATTERN.fullmatch(value):
-            raise ValueError
-
-        return value
-
-    @field_validator("email")
-    def validate_email(cls, value: str) -> str:
-        EMAIL_PATTERN = re.compile(r"[\w\.]+@[\w]+\.[\w]+")
-        if not EMAIL_PATTERN.fullmatch(value):
-            raise ValueError
-
+    @field_validator("*")
+    def validate_field(cls, value: t.Any, info: ValidationInfo) -> t.Any:
+        if validator := FIELD_VALIDATORS.get(info.field_name or ""):
+            validator.validate(value=value)
         return value
 
 
